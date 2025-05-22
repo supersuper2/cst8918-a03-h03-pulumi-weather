bbcef4afbd7ecb4c2416bca80cb65ddbfc89666b
supersuper2 <sing1883@algonquinlive.com>
2025-05-22 18:25:58 -0400

    Define the container registry

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,4 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as resources from '@pulumi/azure-native/resources';
+import * as containerregistry from '@pulumi/azure-native/containerregistry';
 
 // Import the configuration settings for the current stack.
 const config = new pulumi.Config()
@@ -12,3 +14,31 @@ const containerPort = config.requireNumber('containerPort')
 const publicPort = config.requireNumber('publicPort')
 const cpu = config.requireNumber('cpu')
 const memory = config.requireNumber('memory')
+
+// Create a resource group.
+const resourceGroup = new resources.ResourceGroup(`${prefixName}-rg`)
+
+// Create the container registry.
+const registry = new containerregistry.Registry(`${prefixName}ACR`, {
+  resourceGroupName: resourceGroup.name,
+  adminUserEnabled: true,
+  sku: {
+    name: containerregistry.SkuName.Basic,
+  },
+}) 
+
+// Get the authentication credentials for the container registry.
+const registryCredentials = containerregistry
+  .listRegistryCredentialsOutput({
+    resourceGroupName: resourceGroup.name,
+    registryName: registry.name,
+  })
+  .apply((creds) => {
+    return {
+      username: creds.username!,
+      password: creds.passwords![0].value!,
+    }
+  })
+
+export const acrServer = registry.loginServer
+export const acrUsername = registryCredentials.username
