21c0e79738ca75c5d096f66c4138a988a44f5df0
supersuper2 <sing1883@algonquinlive.com>
2025-05-25 16:28:34 -0400

    Update the IaC Definition

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -3,6 +3,7 @@ import * as resources from '@pulumi/azure-native/resources';
 import * as containerregistry from '@pulumi/azure-native/containerregistry';
 import * as dockerBuild from '@pulumi/docker-build';
 import * as containerinstance from '@pulumi/azure-native/containerinstance';
+import * as cache from '@pulumi/azure-native/cache'
 
 // Import the configuration settings for the current stack.
 const config = new pulumi.Config()
@@ -16,6 +17,7 @@ const containerPort = config.requireNumber('containerPort')
 const publicPort = config.requireNumber('publicPort')
 const cpu = config.requireNumber('cpu')
 const memory = config.requireNumber('memory')
+
 
 // Create a resource group.
 const resourceGroup = new resources.ResourceGroup(`${prefixName}-rg`)
@@ -62,6 +64,32 @@ const image = new dockerBuild.Image(`${prefixName}-image`, {
   ],
 })
 
+// Create a managed Redis service
+const redis = new cache.Redis(`${prefixName}-redis`, {
+  name: `${prefixName}-weather-cache`,
+  location: 'westus3',
+  resourceGroupName: resourceGroup.name,
+  enableNonSslPort: true,
+  redisVersion: 'Latest',
+  minimumTlsVersion: '1.2',
+  redisConfiguration: {
+    maxmemoryPolicy: 'allkeys-lru'
+  },
+  sku: {
+    name: 'Basic',
+    family: 'C',
+    capacity: 0
+  }
+})
+
+// Extract the auth creds from the deployed Redis service
+const redisAccessKey = cache
+  .listRedisKeysOutput({ name: redis.name, resourceGroupName: resourceGroup.name })
+  .apply(keys => keys.primaryKey)
+
+  // Construct the Redis connection string to be passed as an environment variable in the app container
+  const redisConnectionString = pulumi.interpolate`rediss://:${redisAccessKey}@${redis.hostName}:${redis.sslPort}`
+
 // Create a container group in the Azure Container App service and make it publicly accessible.
 const containerGroup = new containerinstance.ContainerGroup(
   `${prefixName}-container-group`,
@@ -94,6 +122,10 @@ const containerGroup = new containerinstance.ContainerGroup(
           {
             name: 'WEATHER_API_KEY',
             value: config.requireSecret('weatherApiKey'),
+          },
+          {
+            name: 'REDIS_URL',
+            value: redisConnectionString
           },
         ],
         resources: {
